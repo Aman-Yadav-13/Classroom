5878ce8ca63976a36099cb8bb32d10837b82e01d
Aman-Yadav-13 <aman13092003@gmail.com>
2024-08-14 19:25:45 +0530

    updated server request path in backend

--- server/app.js
+++ server/app.js
@@ -10,7 +10,7 @@ const app = express();
 const mongoURI = 'mongodb+srv://aman13092003:rWyIKMDwZH7OW04g@cluster0.3vl4l.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 app.use(cors({
-    origin: ['http://localhost:1234', 'https://classroom-ozmt.onrender.com'],
+    origin: ['http://localhost:1234', 'https://classroom-coral.vercel.app'],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     allowedHeaders: ['Content-Type'], 
 }));
